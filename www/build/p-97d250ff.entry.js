928c9f9eb23c36f942d81d4872584861442da66b
vitaliy.gavriliyk <vitaliy.gavriliuk@gmail.com>
2020-11-27 19:45:04 +0200

    add: build files

---
+++ www/build/p-97d250ff.entry.js
@@ -0,0 +1,1 @@
+import{r as e,g as t}from"./p-ba77ed68.js";import"./p-aa337fce.js";import{u as s}from"./p-7f3cf776.js";const r=class{constructor(t){e(this,t),this.all=!1}componentWillLoad(){const{el:e,degree:t,selector:r,all:o}=this;s({el:e,degree:t,selector:r,all:o})}get el(){return t(this)}};export{r as uni_tilt_wrap}
