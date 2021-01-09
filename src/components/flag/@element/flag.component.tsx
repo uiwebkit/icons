63a161303bd2cc52b5ee480daaa1e3c1b8a8c95c
UiWebKit <uniwebkit@gmail.com>
2021-01-09 19:51:00 +0200

    improve: flag

--- src/components/flag/@element/flag.component.tsx
+++ src/components/flag/@element/flag.component.tsx
@@ -1,7 +1,6 @@
 import { Component, ComponentInterface, Element, h, Prop, VNode } from '@stencil/core';
 
-import { UniSize } from '../../../models';
-import { isEmpty, uniSmartWrap } from '../../../utils';
+import { isEmpty } from '../../../utils';
 
 @Component({
   tag: 'uni-flag',
@@ -17,16 +16,6 @@ export class UniFlagComponent implements ComponentInterface {
 
   @Prop({ reflect: true }) round: boolean = false;
 
-  @Prop({ reflect: true }) size: UniSize | number;
-
-  @Prop({ reflect: true }) angle: number;
-
-  @Prop({ reflect: true }) spin: boolean = false;
-
-  @Prop({ reflect: true }) speed: number;
-
-  @Prop({ reflect: true }) steps: number;
-
   componentWillLoad(): Promise<void> | void {
     this.name = this.name || this.el.textContent;
     this.el.innerHTML = '';
@@ -36,12 +25,5 @@ export class UniFlagComponent implements ComponentInterface {
     const UniFlagTag = !isEmpty(this.name) ? `uni-flag-${this.name}` : null;
 
     return <UniFlagTag square={this.square || this.round} round={this.round} />;
-  }
-
-  componentDidLoad(): void {
-    const { el, size, angle, spin, speed, steps } = this;
-    const props = { el, size, angle, spin, speed, steps, selector: 'svg' };
-
-    uniSmartWrap(props);
   }
 }
